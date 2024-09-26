0bc272fcb68da044c57a8c556ac006fc15ef3f65
WellisLopesJr-Tech <wellislopesjrofficial@gmail.com>
2024-09-26 20:22:50 -0300

    collections concluida

--- HeroesAPI/Collections/Heroe.cs
+++ HeroesAPI/Collections/Heroe.cs
@@ -13,10 +13,37 @@ public class Heroe
     [BsonRepresentation(BsonType.ObjectId)]
     public string Id { get; set; }
 
-    [BsonElement("descrição")]
-    public string Descricao { get; set; }
+    [BsonElement("nome")]
+    [JsonPropertyName("Nome")]
+    public string Nome { get; set; }
 
-    [BsonElement("")]
+    [BsonElement("time")]
+    [JsonPropertyName("Time")]
+    public string Time { get; set; }
+
+    [BsonElement("idade")]
+    [JsonPropertyName("Idade")]
+    public int Idade { get; set; }
+
+    [BsonElement("genero")]]
+    [JsonPropertyName("Gênero")]
+    public string Genero { get; set; }
+    
+    [BsonElement("habilidades")]
+    [JsonPropertyName("Habilidades")]
+    public List<Habilidades> Habilidades { get; set; }
+
+    [BsonElement("usaCapa")]
+    [JsonPropertyName("Usa Capa")]
+    public bool usaCapa { get; set; }
+
+    [BsonElement("universo")]
+    [JsonPropertyName("Universo")]
+    public string Universo { get; set; }
+    
+
+
+    
 
 
 
